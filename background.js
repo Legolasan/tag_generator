8555963267a3bdba78fd843e9be406864822120f
admin <arunsunderraj@Arun-Sunderraj.local>
2020-08-13 14:11:48 +0530

    background js commented out

--- background.js
+++ background.js
@@ -1,7 +1,7 @@
-document.addEventListener("DOMContentLoaded", () => {
-    var button = document.getElementById("submit")
+// document.addEventListener("DOMContentLoaded", () => {
+//     var button = document.getElementById("submit")
    
-    button.addEventListener("click", (e) => {
-      console.log(e)
-    })
-   })
+//     button.addEventListener("click", (e) => {
+//       console.log(e)
+//     })
+//    })
