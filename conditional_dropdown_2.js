8c5e3051acd95b4644db319409c912f52c38c609
admin <arunsunderraj@Arun-Sunderraj.local>
2022-01-31 20:24:38 +0530

    spam added

--- conditional_dropdown_2.js
+++ conditional_dropdown_2.js
@@ -10,7 +10,8 @@ jQuery(function($){
         'Issue':['--','Delay in data load','Volume of data mismatch','Data load not started','Incorrect structure of data loaded','Issue with transformation','Others'],
         'Query':['--','NA'],
         'Source' : ['--', "Ad Roll","Amazon S3","Amplitude","Apple Search Ads","Appsflyer","Asana","BigQuery","Campaign Manager","Clevertap","Criteo","DataBricks","Dynamo DB","Elastic Search","FB Ads","FB_Pages","FireBase Analytics","FireBolt","Freshdesk","FTP/SFTP","GA360","GA4","Github","Google Ads","Google Clous Storage","Google Drive","Google Play Console","Google Search Console","Google Sheet","Hubspot","IG Business","Intercom","JIRA","Kafka","Klaviyo","Linkedin Ads","Mailchimp","Marketo","Mixpanel","Mongo","MS Ads","Ms-SQL","Mysql","Oracle","Outbrain","Pardot","Postgres","Redshift","REST API","Salesforce","Sendgrid","Shopify","Snowflake","Stripe","Twitter Ads","Webhooks","Xero","Youtube Analytics","Zendesk",'Others'],
-        'Destination' : ['--', "MySQL", "MS-SQL", "Postgres","Redshift", "Snowflake", "BigQuery", "Databricks", "FireBolt", "Amazon S3",'Others']
+        'Destination' : ['--', "MySQL", "MS-SQL", "Postgres","Redshift", "Snowflake", "BigQuery", "Databricks", "FireBolt", "Amazon S3",'Others'],
+        'Spam':['--','Spam']
     }
     var $secondary = $('#secondary');
     $('#primary').change(function (){
